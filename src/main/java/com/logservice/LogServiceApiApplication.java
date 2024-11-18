c87d94e6d23f43dbbe0e805b9a4d376a74419b39
Michael Henrique Cervantes da Silva <michael.cervantes@l8group.net>
2024-11-18 18:33:35 -0300

    settings properties

---
+++ src/main/java/com/logservice/LogServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.logservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logservice/LogServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
